1834512b1162edf62d5b647b658bd9665fc2c099
ZeoZagart <abhashasd1@gmail.com>
2023-07-23 16:24:02 +0530

    cleanup screen

--- src/components/containers/Screen.tsx
+++ src/components/containers/Screen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export function Screen({ children }: {children: React.ReactNode}) {
-    return <div className="flex flex-col w-full h-screen p-4">{children}</div>;
+    return <div className="h-screen">{children}</div>;
 }
